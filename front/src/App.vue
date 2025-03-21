4ac4d1e6eab77610088641ab3e64e2cdd49f7db6
ximass <mateus.schmitz7@hotmail.com>
2025-03-21 13:43:03 -0300

    fix: toasts

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,6 +5,9 @@
     <v-main>
       <router-view />
     </v-main>
+    <v-snackbar v-model="isToastVisible" timeout="3000" :color="toastColor">
+       {{ toastMessage }}
+     </v-snackbar>
   </v-app>
 </template>
 
@@ -13,17 +16,23 @@ import { defineComponent, ref } from 'vue';
 import TopMenu from './components/TopMenu.vue';
 import SideMenu from './components/SideMenu.vue';
 import { useAuth } from './composables/auth';
+import { useToast } from './composables/useToast';
+import './assets/styles/global.css';
 
 export default defineComponent({
   name: 'App',
   components: { TopMenu, SideMenu },
   setup() {
+    const { isToastVisible, toastMessage, toastColor } = useToast();
     const { fetchUser, isAuthenticated, user } = useAuth();
     const drawerOpen = ref(false);
 
     fetchUser();
 
     return {
+      isToastVisible,
+      toastMessage,
+      toastColor,
       isAuthenticated,
       user,
       drawerOpen
